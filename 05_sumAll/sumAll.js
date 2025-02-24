9feb9b0cddced2ba1e25dc0ae020f6f894a7222f
Virtnet <virtnet@hotmail.com>
2025-02-24 19:18:46 +0100

    solution added

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(fromNumber,toNumber) {
+    let total = 0;
+    if(fromNumber <= 0 || toNumber <= 0 || !Number.isInteger(fromNumber) || !Number.isInteger(toNumber)) {
+        return "ERROR";
+    }
+    if(fromNumber > toNumber){
+        let tempNumber = fromNumber;
+        fromNumber = toNumber;
+        toNumber = tempNumber;
 
+    }
+    for(i = fromNumber; i<= toNumber; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
