fad470985db224278506720113d2226e2e284191
Virtnet <virtnet@hotmail.com>
2025-03-01 19:26:56 +0100

    added solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,10 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(valueFah) {
+  return Math.round(((valueFah - 32) * 5/9) * 10 ) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(valueCel) {
+  return Math.round((valueCel * (9/5) + 32) * 10) / 10;
+
 };
 
 // Do not edit below this line
