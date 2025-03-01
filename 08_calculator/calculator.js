6f01129b15a2f4793c7ac747d4601770516931f9
Virtnet <virtnet@hotmail.com>
2025-03-01 20:31:32 +0100

    solution added

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
-const add = function() {
+const add = function(val1,val2) {
+  return val1 + val2;
 	
 };
 
-const subtract = function() {
+const subtract = function(val1,val2) {
+  return val1 - val2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  if(array.length === 0 ) return 0;
+  let total = 0;
+  for(i = 0; i < array.length; i++){
+    total += array[i];
+  }
+  return total;
 	
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  if(array.length === 0) return 0;
+  total = 1;
+  for(i = 0; i< array.length; i++){
+    total *= array[i];
+  }
+  return total;
 
 };
 
-const power = function() {
+const power = function(val1,val2) {
+  let total = val1;
+  for(i=1 ; i < val2; i++){
+    total *= val1;
+  }
+  return total;
 	
 };
 
-const factorial = function() {
-	
+const factorial = function(val1) {
+  let total = 1;
+  if(val1 < 2) return 1;
+  for(i = 2; i< val1 + 1; i++){
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
