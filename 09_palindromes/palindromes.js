43a54f09fa32b539764c969d37e80b53408a961c
Virtnet <virtnet@hotmail.com>
2025-03-01 21:34:48 +0100

    added solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (stringCheck) {
+    var punctuation = /[\.,?! ]/g;
+    cleanedArray = stringCheck.toLowerCase().replace(punctuation,"").split("").join("");
+    arrayReversed = cleanedArray.split("").reverse().join("");
+    // for(let i = 0 ; i < arrayReversed.length + 1 ; i ++){
+    //     if(arrayReversed[i] !== cleanedArray[i] ){
+    //         return false;
+    //     }
+    // }
+    // return true;
+
+    return cleanedArray === arrayReversed;
 
 };
 
